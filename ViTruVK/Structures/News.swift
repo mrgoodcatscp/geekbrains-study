b2f94036129d703e90ac3232c63b494fdd6795e6
mrgoodcatscp <vadim.terekhvo@outlook.com>
2021-05-06 22:45:11 +0600

    Lesson 2

--- ViTruVK/Structures/News.swift
+++ ViTruVK/Structures/News.swift
@@ -7,9 +7,51 @@
 
 import Foundation
 
-struct News {
-    let userPosted: User
-    let text: String
-    let photo: String
-    let like: Bool
+class Attachement: Decodable {
+    
+    var photos: [String]?
+    
 }
+
+class News: Decodable {
+    
+    var id: Int?
+    var date: Date?
+    var text: String?
+    var attachements: [Attachement]?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "source_id"
+        case date
+        case text
+        case attachements
+    }
+}
+
+
+class NewsList: Decodable {
+    
+    let items: [News]
+    let profiles: [User]
+    let groups: [Group]
+    
+    enum CodingKeys: String, CodingKey {
+        case items
+        case profiles
+        case groups
+    }
+}
+
+
+class NewsFeedResponse: Decodable {
+    
+    let response: NewsList
+    
+    enum CodingKeys: String, CodingKey {
+        case response
+    }
+
+}
+
+
+
