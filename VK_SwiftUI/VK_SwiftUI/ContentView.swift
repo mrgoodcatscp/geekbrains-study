ea296d5ed85fd92c2b1c9a58f4d99f78924e2117
mrgoodcatscp <vadim.terekhvo@outlook.com>
2021-12-02 22:02:34 +0300

    SwiftUI_Lesson1

---
+++ VK_SwiftUI/VK_SwiftUI/ContentView.swift
@@ -0,0 +1,111 @@
+//
+//  ContentView.swift
+//  VK_SwiftUI
+//
+//  Created by Вадим Терехов on 28.11.2021.
+//
+
+import SwiftUI
+import Combine
+
+struct ContentView: View {
+    
+    @State private var login: String = ""
+    @State private var password: String = ""
+    @State private var isLogoShowing: Bool = true
+    
+    private var commonTopPadding: CGFloat {
+        isLogoShowing ? 0 : 64
+    }
+    private let textFieldsMaxWidth: CGFloat = 220
+    private let vkStandardColor: String = "VKStandard"
+    private let vkWhiteColor: CGColor = CGColor(red: 255, green: 255, blue: 255, alpha: 1)
+    private let vkLogoName: String = "VK_Monochrome_Full_Logo"
+    private let vkLogoWidth: CGFloat = 287.65625
+    private let vkLogoHeight: CGFloat = 50
+    
+    private let isKeyboardShowingPub = Publishers.Merge(
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification).map { _ in false},
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification).map { _ in true}
+    )
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Color(vkStandardColor)
+                .ignoresSafeArea()
+            VStack {
+                if isLogoShowing {
+                    Image(vkLogoName)
+                        .resizable()
+                        .frame(width: vkLogoWidth, height: vkLogoHeight)
+                        .padding(EdgeInsets(top: 64, leading: 0, bottom: 32, trailing: 0))
+                }
+                
+                VStack {
+                    HStack {
+                        Text("Логин")
+                            .foregroundColor(Color(vkWhiteColor))
+                        Spacer()
+                        TextField("Ведите логин", text: $login)
+                            .frame(maxWidth: textFieldsMaxWidth)
+                    }
+                    HStack {
+                        Text("Пароль")
+                            .foregroundColor(Color(vkWhiteColor))
+                        Spacer()
+                        SecureField("Ведите пароль", text: $login)
+                            .frame(maxWidth: textFieldsMaxWidth)
+                    }
+                }
+                .frame(minWidth: 150, maxWidth: 300)
+                .padding(EdgeInsets(top: commonTopPadding, leading: 0, bottom: 0, trailing: 0))
+                
+                Button(action: self.onLoginButtonPressed) {
+                    HStack {
+                        Text("Войти")
+                            .foregroundColor(Color.white)
+                        Image(systemName: "arrow.right.circle")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(Color.white)
+                    }
+                        
+                }
+                .padding(EdgeInsets(top: 30, leading: 0, bottom: 30, trailing: 0))
+                .disabled(login.isEmpty || password.isEmpty)
+                Spacer()
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+        .onReceive(isKeyboardShowingPub) {value in
+            withAnimation {
+                self.isLogoShowing = !value
+            }
+        }
+        .onTapGesture {
+            endEditing()
+        }
+    }
+    
+    private func onLoginButtonPressed() {
+        
+    }
+    
+    private func endEditing() {
+        UIApplication.shared.endEditing()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension UIApplication {
+    func endEditing() {
+        self.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
---
+++ VK_SwiftUI/VK_SwiftUI/VK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_SwiftUIApp.swift
+//  VK_SwiftUI
+//
+//  Created by Вадим Терехов on 28.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct VK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
